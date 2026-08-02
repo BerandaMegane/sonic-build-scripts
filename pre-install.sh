ac459e151d01996abc5f62e8390781a8ad6e9344
BerandaMegane <68979594+BerandaMegane@users.noreply.github.com>
2026-08-02 05:00:21 +0000

    PVE VM Ubuntu26.04

--- pre-install.sh
+++ pre-install.sh
@@ -2,8 +2,6 @@
 
 # pre-install
 sudo apt update && sudo apt upgrade -y
-sudo apt install -y docker.io
-sudo apt install -y python3-venv
-
+sudo apt install -y docker.io python3-venv j2cli make
 sudo usermod -aG docker ubuntu
 newgrp docker
--- setup.sh
+++ setup.sh
@@ -8,24 +8,19 @@ sudo mkdir -p /var/cache/sonic
 sudo chown ubuntu:root /var/cache/sonic
 
 # Enter the source directory
-cd ~
 git clone https://github.com/sonic-net/sonic-buildimage.git
-cd ~/sonic-buildimage
+cd sonic-buildimage
 git fetch --all --tags
 git checkout 202511
 
 # Python venv
-python3 -m venv .venv
-source .venv/bin/activate
-pip3 install --upgrade pip
-pip3 install jinjanator
+# python3 -m venv .venv
+# source .venv/bin/activate
+# pip3 install --upgrade pip
+# pip3 install jinjanator
 
 # rules/config を置き換える設定
-cp ~/sonic-build-scripts/config.user rules/
-cp ~/sonic-build-scripts/build.sh .
+cp ../config.user rules/
+cp ../build.sh .
 
-# j2cli availability の判定を調整する j2cli -> j2
-sed -i "s/grep j2cli/grep j2/" Makefile.work
-
-cat rules/config.user
-grep J2_VER Makefile.work
+j2 -version
