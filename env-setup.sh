6895fe4e4f1b9c9d2f1fed0d752a184f0a0d9484
BerandaMegane <68979594+BerandaMegane@users.noreply.github.com>
2026-08-23 19:24:40 +0900

    微修正

--- env-setup.sh
+++ env-setup.sh
@@ -3,6 +3,5 @@
 git clone https://github.com/sonic-net/sonic-buildimage.git
 cd sonic-buildimage
 git checkout 202605
-
-cd sonic-buildimage
-SONIC_DIR=$(pwd) bash scripts/prerequisites.sh
+bash scripts/prerequisites.sh
+cd ..
